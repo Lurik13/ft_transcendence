15ac1e3bc2534b89b1f0cb2df296b2b7a52f1442
MXK7 <medixom@gmail.com>
2024-08-27 16:15:02 +0200

    fix style bouton and hover

--- src/components/CreateButton.vue
+++ src/components/CreateButton.vue
@@ -1,22 +1,35 @@
 <script setup> </script>
 
 <template>
-    <button class="button">Create</button>
+    <button class="button">
+        <text class="buttonText">
+            Play
+        </text>
+    </button>
 </template>
 
 <style>
 .button {
-    background-color: #4CAF50;
-    opacity: 0.7;
+    background-color: rgba(255, 255, 255, 0.0);
     padding: 1rem 2rem;
-    border-radius: 0.5rem;
-    border: rgb(255, 166, 0);
+    border: 4px solid rgba(255, 255, 255, 0.5);
+    border-radius: 0.4vw;
+    border-width: 0.2vw;
+    transition: border-color 0.5s;
+}
+
+.buttonText {
+    color: rgb(255, 255, 255);
+    padding: 1rem 2rem;
     font-size: 1.25rem;
     font-weight: 600;
     cursor: pointer;
+    opacity: 1;
 }
 
 .button:hover {
-    opacity: 1;
+    border-color: rgba(255, 255, 255, 1);
+    transition: border-color 0.5s;
 }
+
 </style>
