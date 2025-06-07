a6cf9dd73b2d670405bcd7983c52c3961869507b
Lurik13 <lucasribette.pro@gmail.com>
2025-06-07 11:26:57 +0200

    cors failed

--- django/pong/settings.py
+++ django/pong/settings.py
@@ -43,6 +43,8 @@ CSRF_TRUSTED_ORIGINS = ['https://localhost:8443',
                         'https://10.11.*.*:8443',
                         'https://10.11.7.13:8443',
                         'https://82.25.116.35:8443',
+                        'https://lucasribette.com:8443',
+                        'https://lucasribette.com',
                         ]
 
 CSRF_COOKIE_HTTPONLY = False
@@ -58,6 +60,8 @@ CORS_ALLOWED_ORIGINS = [
     'https://10.11.*.*:8443',
     'https://10.11.7.13:8443',
     'https://82.25.116.35:8443',
+    'https://lucasribette.com:8443',
+    'https://lucasribette.com',
 ]
 
 
